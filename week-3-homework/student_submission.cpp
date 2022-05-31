f92a7c1ece866a61235ab5328d56c94a2b523a39
parallels <chengxuyuan1998@gmail.com>
2022-05-31 15:21:35 +0200

    dynamic schedule and reduction

--- week-3-homework/student_submission.cpp
+++ week-3-homework/student_submission.cpp
@@ -29,9 +29,7 @@ int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
     int pointsInSetCount = 0;
 
 // For each pixel in the image
-#pragma omp parallel num_threads(32)
-    {
-#pragma omp for collapse(2) schedule(static, 7000)
+#pragma omp parallel for schedule(dynamic) reduction(+: pointsInSetCount)
         for (int i = 0; i < y_resolution; i++)
             for (int j = 0; j < x_resolution; j++)
             {
@@ -57,7 +55,6 @@ int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
                 // were before we confirmed the divergence.
                 if (k == max_iter)
                 {
-#pragma omp atomic
                     pointsInSetCount++;
                 }
 
@@ -74,7 +71,6 @@ int mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
                     }
                 }
             }
-    }
 
     return pointsInSetCount;
 }
