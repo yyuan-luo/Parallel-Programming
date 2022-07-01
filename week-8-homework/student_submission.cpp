d7282d354161b49459d06b167d24b5429ca16321
yuan <chengxuyuan1998@gmail.com>
2022-07-01 09:55:43 +0200

    week-8: add some thoughs and implement the for-loop based on rank

--- week-8-homework/student_submission.cpp
+++ week-8-homework/student_submission.cpp
@@ -8,40 +8,43 @@
 #include "life.h"
 #include "Utility.h"
 #include "VideoOutput.h"
+#include <mpi.h>
+#include <math.h>
 
 #define CODE_VERSION 1
+
 /*
   Apply the game of life rules on a Torus --> grid contains shadow rows and columns
   to simplify application of rules i.e. grid actually ranges from grid [ 1.. height - 2 ][ 1 .. width - 2]
 */
-void evolve(ProblemData& problemData) {
-    auto& grid = *problemData.readGrid;
-    auto& writeGrid = *problemData.writeGrid;
+void evolve(ProblemData &problemData, int rank, int block_width, int sqrt_size) {
+    auto &grid = *problemData.readGrid;
+    auto &writeGrid = *problemData.writeGrid;
+    // TODO: MPI_Send and MPI_Recv to be implemented
     // For each cell
-    for (int i = 1; i < GRID_SIZE - 1; i++) {
-        for (int j = 1; j < GRID_SIZE - 1; j++) {
+    for (int x = max(1, (rank % block_num_horizontal) * block_width);
+         x < min(GRID_SIZE - 1, (rank % block_num_horizontal + 1) * block_width); x++) {
+        for (int y = max(1, (rank / block_num_horizontal) * block_width);
+             y < min(GRID_SIZE - 1, (rank / block_num_horizontal + 1) * block_width); y++) {
             // Calculate the number of neighbors
             int sum = grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] +
-                grid[i][j - 1] + grid[i][j + 1] +
-                grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1];
+                      grid[i][j - 1] + grid[i][j + 1] +
+                      grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1];
 
             if (!grid[i][j]) {
                 // If a cell is dead, it can start living by reproduction or stay dead
                 if (sum == 3) {
                     // reproduction
                     writeGrid[i][j] = true;
-                }
-                else {
+                } else {
                     writeGrid[i][j] = false;
                 }
-            }
-            else {
+            } else {
                 // If a cell is alive, it can stay alive or die through under/overpopulation
                 if (sum == 2 || sum == 3) {
                     // stays alive
                     writeGrid[i][j] = true;
-                }
-                else {
+                } else {
                     // dies due to under or overpopulation
                     writeGrid[i][j] = false;
                 }
@@ -75,19 +78,44 @@ void copy_edges(bool(&grid)[GRID_SIZE][GRID_SIZE]) {
     grid[GRID_SIZE - 1][0] = grid[1][GRID_SIZE - 2];
 }
 
-int main(int argc, char** argv) {
+int count_alive(ProblemData &data, int rank, int block_width, int sqrt_size) {
+    auto &grid = *data.readGrid;
+    int counter = 0;
+    // TODO: change range based on rank
+    for (int x = max(1, (rank % block_num_horizontal) * block_width);
+         x < min(GRID_SIZE - 1, (rank % block_num_horizontal + 1) * block_width); x++) {
+        for (int y = max(1, (rank / block_num_horizontal) * block_width);
+             y < min(GRID_SIZE - 1, (rank / block_num_horizontal + 1) * block_width); y++) {
+            if (grid[x][y]) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
+int main(int argc, char **argv) {
     bool activateVideoOutput = false;
     if (argc > 1) {
         if (argc == 2 && strcmp(argv[1], "-g") == 0) {
             activateVideoOutput = true;
-        }
-        else {
+        } else {
             fprintf(stderr, "Usage:\n  %s [-g]\n    -g: Activate graphical output.\n", argv[0]);
             exit(153);
         }
     }
+    MPI_Init(&argc, &argv);
+    int size, rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    auto* problemData = new ProblemData;
+    int sqrt_size = sqrt(size)
+    int block_width = GRID_SIZE / sqrt_size;
+
+    int local_sum = 0, global_sum = 0;
+    auto *problemData = new ProblemData;
+
+    copy_edges(*problemData->readGrid);
 
     // As with Jack Sparrow's exercise, this needs FFMPEG (new and improved: this now works with more video players).
     // As an alternative, you can write individual png files to take a look at the data.
@@ -100,14 +128,15 @@ int main(int argc, char** argv) {
 
     //TODO@Students: This is the main simulation. Parallelize it using MPI.
     for (int iteration = 0; iteration < NUM_SIMULATION_STEPS; ++iteration) {
-        copy_edges(*problemData->readGrid);
-
-        if (activateVideoOutput) {
-            VideoOutput::writeVideoFrames(*problemData);
-        }
 
         if (iteration % SOLUTION_REPORT_INTERVAL == 0) {
-            Utility::outputIntermediateSolution(iteration, *problemData);
+            // TODO: how to make sure when try to printf, every process stays at SOLUTION_REPORT_INTERVAL
+            //  or that you can get the count info for this interval
+
+            /** might be possible to send the local _sum info to one process, since MPI_send is blocking
+             * or apply MPI_Isend(Non-blocking) to store this info of each process in one process and printed
+             * by that process */
+            local_sum = count_alive(problemData, rank, block_width, sqrt_size);
         }
 
         evolve(*problemData);
@@ -123,6 +152,7 @@ int main(int argc, char** argv) {
     }
 
     delete problemData;
+    MPI_Finalize();
     return 0;
 }
 
