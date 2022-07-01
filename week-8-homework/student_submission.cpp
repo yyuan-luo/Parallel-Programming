65d44f51c409f4051bd2526f511bb0633242a459
parallels <chengxuyuan1998@gmail.com>
2022-07-01 15:40:41 +0200

    week-8: checkpoint from mac

--- week-8-homework/student_submission.cpp
+++ week-8-homework/student_submission.cpp
@@ -32,22 +32,23 @@ void evolve(ProblemData &problemData, int rank, int block_width, int sqrt_size) 
         MPI_Recv(&grid[0][1], block_width - 1, MPI_CXX_BOOL, 2, 1, MPI_COMM_WORLD, nullptr);
 
         // left
-        bool tmp[block_width - 1];
+        bool send[block_width - 1] = malloc(sizeof(bool) * (block_width - 1));
+        bool receive[block_width - 1] = malloc(sizeof(bool) * (block_width - 1));
         for (int i = 1; i < block_width; ++i)
-            tmp[i - 1] = grid[i][1];
+            send[i - 1] = grid[i][1];
         MPI_Send(tmp, block_width - 1, MPI_CXX_BOOL, 1, 2, MPI_COMM_WORLD);
-        MPI_Recv(tmp, block_width - 1, MPI_CXX_BOOL, 1, 2, MPI_COMM_WORLD, nullptr);
+        MPI_Recv(receive, block_width - 1, MPI_CXX_BOOL, 1, 2, MPI_COMM_WORLD, nullptr);
         for (int i = 1; i < block_width; ++i)
-            grid[i][0] = tmp[i - 1];
+            grid[i][0] = receive[i - 1];
 
         // right
         for (int i = 1; i < block_width; ++i) {
-            tmp[i - 1] = grid[i][block_width - 1];
+            send[i - 1] = grid[i][block_width - 1];
         }
-        MPI_Send(&tmp, block_width - 1, MPI_CXX_BOOL, 1, 3, MPI_COMM_WORLD);
-        MPI_Recv(tmp, block_width - 1, MPI_CXX_BOOL, 1, 3, MPI_COMM_WORLD, nullptr);
+        MPI_Send(send, block_width - 1, MPI_CXX_BOOL, 1, 3, MPI_COMM_WORLD);
+        MPI_Recv(receive, block_width - 1, MPI_CXX_BOOL, 1, 3, MPI_COMM_WORLD, nullptr);
         for (int i = 1; i < block_width; ++i) {
-            grid[block_width][i] = tmp[i - 1];
+            grid[i][block_width] = receive[i - 1];
         }
 
         // bottom
@@ -55,45 +56,42 @@ void evolve(ProblemData &problemData, int rank, int block_width, int sqrt_size) 
         MPI_Recv(&grid[block_width][1], block_width - 1, MPI_CXX_BOOL, 2, 4, MPI_COMM_WORLD, nullptr);
 
     } else if (rank == 1) {
+        // top-right corner
         MPI_Send(&grid[1][GRID_SIZE - 2], 1, MPI_CXX_BOOL, 2, 0, MPI_COMM_WORLD);
-#if DEBUG
-        std::cout << "rank " << rank << " sent " << grid[1][GRID_SIZE - 2] << std::endl;
-#endif
+        MPI_Recv(&grid[0][GRID_SIZE - 1], 1, MPI_CXX_BOOL, 2, 0, MPI_COMM_WORLD, nullptr);
 
-        MPI_Recv(&grid[0][GRID_SIZE - 1], 1, MPI_CXX_BOOL, 2, 1, MPI_COMM_WORLD, nullptr);
-#if DEBUG
-        std::cout << "rank " << rank << " received " << grid[0][GRID_SIZE - 1] << std::endl;
-#endif
-        bool tmp[GRID_SIZE - 1];
-        for (int i = 1; i < GRID_SIZE; ++i) {
-            tmp[i - 1] = grid[GRID_SIZE - 2][i];
+        // top
+        MPI_Send(&grid[1][block_width], block_width - 1, MPI_CXX_BOOL, 3, 1, MPI_COMM_WORLD);
+        MPI_Recv(&grid[0][block_width], block_width - 1, MPI_CXX_BOOL, 3, 1, MPI_COMM_WORLD, nullptr);
+
+        // left
+        bool send[block_width - 1] = malloc(sizeof(bool) * (block_width - 1));
+        bool receive[block_width - 1] = malloc(sizeof(bool) * (block_width - 1));
+        MPI_Recv(receive, block_width - 1, MPI_CXX_BOOL, 0, 2, MPI_COMM_WORLD, nullptr);
+        for (int i = 1; i < block_width; ++i)
+            send[i - 1] = grid[i][block_width];
+        MPI_Send(send, block_width - 1, MPI_CXX_BOOL, 0, 2, MPI_COMM_WORLD);
+        for (int i = 1; i < block_width; ++i)
+            grid[i][block_width - 1] = receive[i - 1];
+
+        // right
+        MPI_Recv(receive, block_width - 1, MPI_CXX_BOOL, 0, 3, MPI_COMM_WORLD, nullptr);
+        for (int i = 1; i < block_width; ++i) {
+            send[i - 1] = grid[i][GRID_SIZE - 2];
         }
-        MPI_Send(&tmp, block_width - 1, MPI_CXX_BOOL, 0, 3, MPI_COMM_WORLD);
+        MPI_Send(send, block_width - 1, MPI_CXX_BOOL, 0, 3, MPI_COMM_WORLD);
+        for (int i = 1; i < block_width; ++i) {
+            grid[i][GRID_SIZE - 1] = receive[i - 1];
+        }
 
-
-        MPI_Send(&grid[0][GRID_SIZE], block_width - 1, MPI_CXX_BOOL, 4, MPI_COMM_WORLD);
-
+        // bottom
+        MPI_Recv(&grid[block_width][0], block_width - 1, MPI_CXX_BOOL, 3, 4, MPI_COMM_WORLD, nullptr);
+        MPI_Send(&grid[block_width - 1][block_width - 1], block_width - 1, MPI_CXX_BOOL, 3, 4, MPI_COMM_WORLD);
 
     } else if (rank == 2) {
-        MPI_Recv(&grid[GRID_SIZE - 1][0], 1, MPI_CXX_BOOL, 1, 0, MPI_COMM_WORLD, nullptr);
-#if DEBUG
-        std::cout << "rank " << rank << " sent " << grid[GRID_SIZE - 1][0] << std::endl;
-#endif
 
-        MPI_Send(&grid[GRID_SIZE - 2][1], 1, MPI_CXX_BOOL, 1, 1, MPI_COMM_WORLD);
-#if DEBUG
-        std::cout << "rank " << rank << " received " << grid[GRID_SIZE - 2][1] << std::endl;
-#endif
     } else if (rank == 3) {
-        MPI_Recv(&grid[GRID_SIZE - 1][GRID_SIZE - 1], 1, MPI_CXX_BOOL, 0, 0, MPI_COMM_WORLD, nullptr);
-#if DEBUG
-        std::cout << "rank " << rank << " sent " << grid[GRID_SIZE - 1][GRID_SIZE - 1] << std::endl;
-#endif
 
-        MPI_Send(&grid[GRID_SIZE - 2][GRID_SIZE - 2], 1, MPI_CXX_BOOL, 0, 1, MPI_COMM_WORLD);
-#if DEBUG
-        std::cout << "rank " << rank << " received " << grid[GRID_SIZE - 2][GRID_SIZE - 2] << std::endl;
-#endif
     }
     // For each cell
     for (int i = std::max(1, (rank % sqrt_size) * block_width);
